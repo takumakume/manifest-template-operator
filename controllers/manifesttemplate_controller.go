e1a3bd520694b8c7bd3b575f0fbff405bee3761e
久米 拓馬 <takuma.kume@pepabo.com>
2022-11-21 10:47:51 +0900

    tmp

--- controllers/manifesttemplate_controller.go
+++ controllers/manifesttemplate_controller.go
@@ -18,11 +18,26 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
 
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+
+	serializeryaml "k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	manifesttemplatev1alpha1 "github.com/takumakume/manifest-template-operator/api/v1alpha1"
 )
@@ -49,7 +64,80 @@ type ManifestTemplateReconciler struct {
 func (r *ManifestTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	restConfig, err := rest.InClusterConfig()
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	clientSet, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	discoveryClient := clientSet.Discovery()
+
+	groupResources, err := restmapper.GetAPIGroupResources(discoveryClient)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	mapper := restmapper.NewDiscoveryRESTMapper(groupResources)
+
+	dynamicClient, err := dynamic.NewForConfig(restConfig)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// TODO
+	manifest := ""
+
+	decoder := yaml.NewYAMLOrJSONDecoder(strings.NewReader(manifest), 100)
+	for {
+		//var rawObj runtime.RawExtension
+		var rawObj []byte
+		if err := decoder.Decode(&rawObj); err != nil {
+			fmt.Println("end of contents")
+			break
+		}
+
+		obj := &unstructured.Unstructured{}
+
+		// Create client (client, err := dynamic.NewClient(rawObj.Raw, obj))
+		dec := serializeryaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+		_, gvk, err := dec.Decode(rawObj, nil, obj)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		var client dynamic.ResourceInterface
+		if mapping.Scope.Name() == meta.RESTScopeNameNamespace {
+			if obj.GetNamespace() == "" {
+				// TODO: set namespace
+				obj.SetNamespace(metav1.NamespaceDefault)
+			}
+			client = dynamicClient.Resource(mapping.Resource).Namespace(obj.GetNamespace())
+		} else {
+			client = dynamicClient.Resource(mapping.Resource)
+		}
+
+		// Apply (res, err := dynamic.Apply(client, obj))
+		data, err := json.Marshal(obj)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		opt := metav1.PatchOptions{FieldManager: "example"}
+		o, err := client.Patch(ctx, obj.GetName(), types.ApplyPatchType, data, opt)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		fmt.Printf("applied: %+v\n", o)
+	}
 
 	return ctrl.Result{}, nil
 }
